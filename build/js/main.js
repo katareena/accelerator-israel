e177396711fcda11bd2173d0178a7912be0bb14d
katareena <w10160177@gmail.com>
2020-11-27 17:09:51 +0300

    fix bugs

--- build/js/main.js
+++ build/js/main.js
@@ -340,57 +340,56 @@
     }
   }
 
-	var xDown = null;
-	var yDown = null;
+  var xDown = null;
+  var yDown = null;
 
-	function getTouches(evt) {
-	  return evt.touches || evt.originalEvent.touches;
-	};
+  function getTouches(evt) {
+    return evt.touches || evt.originalEvent.touches;
+  };
 
-	function handleTouchStart(evt) {
-		const firstTouch = getTouches(evt)[0];
-		xDown = firstTouch.clientX;
-		yDown = firstTouch.clientY;
-	};
+  function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+  };
 
-	function handleTouchMove(evt) {
-		if ( ! xDown || ! yDown ) {
-			return;
-		}
+  function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+      return;
+    }
 
-		var xUp = evt.touches[0].clientX;
-		var yUp = evt.touches[0].clientY;
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
 
-		var xDiff = xDown - xUp;
-		var yDiff = yDown - yUp;
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
 
-		if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-			if ( xDiff > 0 ) {
-				/* left swipe */
-				console.log('left');
-				plusSlide(evt);
-			} else {
-				/* right swipe */
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+      if ( xDiff > 0 ) {
+        /* left swipe */
+        console.log('left');
+        plusSlide(evt);
+      } else {
+        /* right swipe */
         console.log('right');
         minusSlide(evt);
-			}
-		} else {
-			if ( yDiff > 0 ) {
-				/* up swipe */
-				console.log('up');
-			} else {
-				/* down swipe */
-				console.log('down');
-			}
-		}
-		/* reset values */
-		xDown = null;
-		yDown = null;
+      }
+    } else {
+      if ( yDiff > 0 ) {
+        /* up swipe */
+        console.log('up');
+      } else {
+        /* down swipe */
+        console.log('down');
+      }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
   };
 
   document.addEventListener('touchstart', handleTouchStart, false);
-	document.addEventListener('touchmove', handleTouchMove, false);
-
+  document.addEventListener('touchmove', handleTouchMove, false);
 })();
 
 'use strict';
